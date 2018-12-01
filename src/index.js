ed1b9504412044713e691c6cd456c077522d4479
Igor Knop <igorknop@gmail.com>
2018-12-01 05:26:34 -0200

    Try a basic draw

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 function component() {
-    let element = document.createElement("div");
-    element.innerHTML = "Hello LDJAM!";
+    let element = document.createElement("canvas");
+    element.width = 320;
+    element.height = 600;
     return element;
 }
 
-document.body.appendChild(component());
+function drawBackground(ctx, canvas){
+    ctx.fillStyle = "black";
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+}
+
+const canvas = component();
+const ctx = canvas.getContext("2d");
+
+document.body.appendChild(canvas);
+drawBackground(ctx, canvas);
